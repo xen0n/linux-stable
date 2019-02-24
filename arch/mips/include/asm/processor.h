fd2f5b36caa2fe2c2bef480f9110dd7c2e273c9d
Wang Xuerui <wangxuerui@qiniu.com>
2019-02-24 12:44:37 +0800

    MIPS: define virtual address size in Kconfig
    To ease addition of new VA sizes, punt the constant in processor.h
    to Kconfig.
    
    Signed-off-by: Wang Xuerui <wangxuerui@qiniu.com>
    Cc: Huacai Chen <chenhc@lemote.com>
    Cc: Alex Belits <alex.belits@cavium.com>
    Cc: James Hogan <james.hogan@mips.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org

--- arch/mips/include/asm/processor.h
+++ arch/mips/include/asm/processor.h
@@ -58,7 +58,7 @@ extern unsigned int vced_count, vcei_count;
  */
 #define TASK_SIZE32	0x7fff8000UL
 #ifdef CONFIG_MIPS_LARGE_VA
-#define TASK_SIZE64     (0x1UL << min(cpu_data[0].vmbits, 48))
+#define TASK_SIZE64     (0x1UL << min(cpu_data[0].vmbits, CONFIG_MIPS_VA_BITS))
 #else
 #define TASK_SIZE64     0x10000000000UL
 #endif
