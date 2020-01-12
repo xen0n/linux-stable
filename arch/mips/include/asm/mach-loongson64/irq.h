60005747e63e92aea91f6a90f7a022ce8f1dc0da
Jiaxun Yang <jiaxun.yang@flygoat.com>
2020-01-12 16:14:15 +0800

    MIPS: Loongson64: Add generic dts
    Add generic device dts for Loongson-3 devices.
    They seems identical but will be different later.
    Some PCH devices like PCI Host Bridge is still enabled by platform
    code for now.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- arch/mips/include/asm/mach-loongson64/irq.h
+++ arch/mips/include/asm/mach-loongson64/irq.h
@@ -4,8 +4,9 @@
 
 #include <boot_param.h>
 
+#define NR_IRQS 512
 /* cpu core interrupt numbers */
-#define MIPS_CPU_IRQ_BASE 56
+#define MIPS_CPU_IRQ_BASE 16
 
 #include_next <irq.h>
 #endif /* __ASM_MACH_LOONGSON64_IRQ_H_ */
